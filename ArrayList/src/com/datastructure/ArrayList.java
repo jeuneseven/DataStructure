835e918311887d68f853332b5efa7fbac2c6c99f
kkk <jeuneseven@gmail.com>
2020-01-30 21:36:31 +0800

    动态数组 init

--- ArrayList/src/com/datastructure/ArrayList.java
+++ ArrayList/src/com/datastructure/ArrayList.java
@@ -1,5 +1,132 @@
 package com.datastructure;
 
 public class ArrayList {
+    /**
+     * 元素的数量
+     */
+    private int size;
+    /**
+     * 所有的元素
+     */
+    private int[] elements;
+    //final相当于const
+    private static final int DEFAULT_CAPACITY = 10;
+    private static final int ELEMENT_NOT_FOUND = -1;
 
+    public ArrayList(int capaticy) {
+        capaticy = (capaticy < DEFAULT_CAPACITY) ? DEFAULT_CAPACITY : capaticy;
+        elements = new int[capaticy];
+    }
+
+    public ArrayList() {
+        //构造函数之间相互调用通过this
+        this(DEFAULT_CAPACITY);
+    }
+
+    /**
+     * 清除所有元素
+     */
+    public void clear() {
+        /**     如果觉得有些浪费可以这样实现
+         *          if (size <= 100) {
+         *             size = 0;
+         *         } else {
+         *             elements = null;
+         *         }
+         */
+        //高效利用空间和时间，无需清空真正内存，不会造成内存浪费
+        size = 0;
+    }
+
+    /**
+     * 元素的数量
+     * @return
+     */
+    public int size() {
+        return size;
+    }
+
+    /**
+     * 是否为空
+     * @return
+     */
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    /**
+     * 是否包含某个元素
+     * @param element
+     * @return
+     */
+    public boolean contains(int element) {
+        return indexOf(element) != ELEMENT_NOT_FOUND;
+    }
+
+    /**
+     * 添加元素到尾部
+     * @param element
+     */
+    public void add(int element) {
+
+    }
+
+    /**
+     * 获取index位置的元素
+     * @param index
+     * @return
+     */
+    public int get(int index) {
+        if (index < 0 || index >= size) {
+            //抛出异常
+            throw new IndexOutOfBoundsException("Index:" + index + ", Size:" + size);
+        }
+        return elements[index];
+    }
+
+    /**
+     * 设置index位置的元素
+     * @param index
+     * @param element
+     * @return 原来的元素ֵ
+     */
+    public int set(int index, int element) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index:" + index + ", Size:" + size);
+        }
+
+        int old = elements[index];
+        elements[index] = element;
+        return old;
+    }
+
+    /**
+     * 在index位置插入一个元素
+     * @param index
+     * @param element
+     */
+    public void add(int index, int element) {
+
+    }
+
+    /**
+     * 删除index位置的元素
+     * @param index
+     * @return
+     */
+    public int remove(int index) {
+        return 0;
+    }
+
+    /**
+     * 查看元素的索引
+     * @param element
+     * @return
+     */
+    public int indexOf(int element) {
+        for (int i = 0; i < size; i++) {
+            if (elements[i] == element) return i;
+        }
+        return ELEMENT_NOT_FOUND;
+    }
 }
