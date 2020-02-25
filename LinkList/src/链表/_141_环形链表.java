4e0cc0d61ac6ce8bcde3f2ccd2f093b294091e4f
kkk <jeuneseven@gmail.com>
2020-02-25 08:44:30 +0800

    Update _141_环形链表.java

--- LinkList/src/链表/_141_环形链表.java
+++ LinkList/src/链表/_141_环形链表.java
@@ -1,4 +1,26 @@
 package 链表;
 
-public class _141_环形链表 {
+import com.jeuneseven.List;
+
+public class _141_环形链表 {//思路是使用快慢指针
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return false;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head.next;
+
+        while (fast != null && fast.next != null) {
+            if (slow == fast) {
+                return true;
+            }
+            slow = slow.next;
+            fast = fast.next.next;
+
+            return false;
+        }
+
+        return false;
+    }
 }
