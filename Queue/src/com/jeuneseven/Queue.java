622474ac0eef5da70c2e4c90cbf640fa27761edf
kkk <jeuneseven@gmail.com>
2020-03-09 21:40:21 +0800

    queue init

---
+++ Queue/src/com/jeuneseven/Queue.java
@@ -0,0 +1,38 @@
+package com.jeuneseven;
+
+/**
+ * 优先使用双向链表实现，因为是频繁在头尾两部分添加删除元素
+ * 练习：https://leetcode-cn.com/problems/implement-queue-using-stacks/
+ * 使用两个栈来实现队列，入队时push到inStack中，出队时如果outStack为空，将inStack中所有元素逐一弹出，push到outStack，outStack弹出栈顶元素
+ * 如果outStack不为空，outStack弹出栈顶元素
+ * @param <E>
+ */
+public class Queue<E>  {
+    public int size() {
+        return 0;
+    }
+    public boolean isEmpty() {
+        return false;
+    }
+
+    /**
+     * 入队
+     * @param Element
+     */
+    public void enQueue(E Element) {
+
+    }
+    /**
+     * 出队
+     * @return
+     */
+    public E deQueue() {
+        return null;
+    }
+    /**
+     * 获取队列头部元素
+     */
+    public E front() {
+        return null;
+    }
+}
